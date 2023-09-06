6b4829f914337de4bbee16a2925674fca6e29637
Danny Desktop Ubuntu <okinawanspud@gmail.com>
2023-09-05 22:54:21 -0500

    complete sharpe ratio calculation

--- quantitativetradingdemos/3_4_calculating_sharpe_ratios.py
+++ quantitativetradingdemos/3_4_calculating_sharpe_ratios.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # read ige data into dataframe
 ige_ = pd.read_csv('data/3_4_ige.csv')
@@ -9,11 +10,21 @@ ige_['Date']=pd.to_datetime(ige_['Date'])
 # sort by Date ascending
 ige_.sort_values(by='Date',inplace=True)
 
-# add Dailyret
-ige_["Dailyret"]=0.0
-
 # populate Dailyret values
-#todo https://stackoverflow.com/questions/16476924/how-to-iterate-over-rows-in-a-dataframe-in-pandasgit
+ige_["PrevClose"] = ige_["Adj Close"].shift(1)
+ige_["Dailyret"]=(ige_["Adj Close"]-ige_["PrevClose"])/ige_["PrevClose"]
 
+# excess excedes adjusted annual 4%
+ige_["ExcessDailyRet"]=ige_["Dailyret"]-(0.04/252)
+
+# calculate Sharpe Ratio
+avg_ = ige_["ExcessDailyRet"].mean()
+std_ = ige_["ExcessDailyRet"].std()
+sharpe_ = np.sqrt(252) * avg_ / std_
+
+# print data
 print(ige_.to_string())
 print(ige_.info())
+print("avg_:"+str(avg_))
+print("std_:"+str(std_))
+print(sharpe_)
