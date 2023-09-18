19138ce4a38bfa092fb3c4eed92ce38aa230ae27
Danny Desktop Ubuntu <okinawanspud@gmail.com>
2023-09-17 20:55:42 -0500

    remove preexisting records

--- app/datagetter.py
+++ app/datagetter.py
@@ -33,8 +33,31 @@ def insert_TIME_SERIES_INTRADAY(data):
   j_ = json.loads(data)
   ticker_ = j_["Meta Data"]["2. Symbol"]
   refresh_ = j_["Meta Data"]["3. Last Refreshed"]
+  checked_preexisting_ = False
   for datetime_ , data in j_["Time Series (5min)"].items():
     mycursor = mydb.cursor()
+
+    # remove preexisting records that match ticker & date
+    if checked_preexisting_ == False:
+      date_ = datetime_[:10]
+      cmd_ = "SELECT datetime FROM "+INTRADAY_TABLE_
+      cmd_ += " WHERE DATE(datetime) = \""+date_+"\""
+      cmd_ += " AND ticker = \""+ ticker_+"\""
+      cmd_ += ";"
+      mycursor.execute(cmd_)
+
+      myresult = mycursor.fetchall()
+      record_count_ = len(myresult)
+      print(record_count_)
+      if(record_count_ > 0):
+        cmd_ = "DELETE FROM " +INTRADAY_TABLE_
+        cmd_ += " WHERE ticker = \"" + ticker_ + "\" "
+        cmd_ += "AND DATE(datetime) = \"" + date_ + "\";"
+        mycursor.execute(cmd_)
+        mydb.commit()
+      checked_preexisting_ = True
+
+
 
     cmd_ = "INSERT INTO " + INTRADAY_TABLE_
     cmd_ += "(ticker, datetime, open, high, low, close, volume, refresh) "
