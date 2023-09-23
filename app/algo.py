bc1d228dd04544cc73d62d666474ba3fd20ffb75
Danny Desktop Ubuntu <okinawanspud@gmail.com>
2023-09-22 22:04:39 -0500

    simulate&analyze original integration, simple simulation implementation

--- app/algo.py
+++ app/algo.py
@@ -12,12 +12,17 @@ class Transaction:
     self.time_ = time
     self.value_ = value
     self.transaction_ = transaction
-
+  def print(self):
+    print(self.time_ +": "+self.transaction_+" at "+str(self.value_))
 class Algo:
   transactions_ = []
   ticker_=""
+  shares_ = 0
+  df_ = pd.DataFrame()
+  datetimes_ = []
   def __init__(self,ticker):
     self.ticker_=ticker
+    self.shares_=0
   def getData(self, time_start="", time_end=""):
     # retrieve data from database to populate dataframe
     time_start_ = time_start
@@ -27,7 +32,6 @@ class Algo:
     if time_end_ == "":
       time_end_ = datetime.now().isoformat()[:18]
 
-    # mycursor = databaseconnector.mydb.cursor()
     cmd_ = "SELECT * FROM " + databaseconnector.INTRADAY_TABLE_
     cmd_ += " WHERE ticker=\""+self.ticker_+"\""
     cmd_ += " AND datetime >= \""+time_start_+"\""
@@ -40,20 +44,32 @@ class Algo:
     db_connection_str+="/"+databaseconnector.database_
     db_connection = create_engine(db_connection_str)
 
-    df_ = pd.read_sql(cmd_, db_connection)
-    print(df_)
+    self.df_ = pd.read_sql(cmd_, db_connection)
 
-  def process(self, time_end=None):
+  def process(self, time_end):
     # process data and generate a position at time_end (end of data by default)
-    pass
+    latest_ = self.df_[(self.df_['datetime'] == time_end)]
+    val_ = latest_.loc[0]["close"] # get 0 index, "close" attribute
+
+    if self.shares_== 0:
+      self.buy(time_end,val_)
+
+
   def buy(self, time, value):
     self.transactions_.append(Transaction(time, value, "buy"))
+    self.shares_+=1
+
   def sell(self, time, value):
     self.transactions_.append(Transaction(time, value, "sell"))
+  def simulate(self):
+    self.process('2023-09-15 19:59:00') # TODO: iterate over all items in pd_
   def analyze(self):
     # compute metrics (profit/loss)
-    pass
+    for t in self.transactions_:
+      t.print()
 
 if __name__ == "__main__":
   a_ = Algo("IBM")
   a_.getData()
+  a_.simulate()
+  a_.analyze()
