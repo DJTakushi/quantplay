29a96a89ee5e4ccac9740d5c7f3eec6a433eaa08
Danny Desktop Ubuntu <okinawanspud@gmail.com>
2023-09-23 21:05:40 -0500

    todo commments; init to None; pre _process & post_process

--- app/algo.py
+++ app/algo.py
@@ -3,9 +3,9 @@ from datetime import datetime
 import pandas as pd
 from sqlalchemy import create_engine
 import time
-# TODO: refactor so processing does not need parse data to find itself
-  # this should be done by someone else so processing can stick to processing 
 # TODO: move daily tracker out to another class to keep Algo single-use
+# TODO: relocate time measurement into functions for better organization of main
+# TODO: move simulator to another class to keep Algo single-use
 class Transaction:
   time_ = None
   value_ = 0.00
@@ -20,10 +20,10 @@ class Transaction:
 
 class DayData:
   date=""
-  open=0.0
-  high=0.0
-  low=0.0
-  close=0.0
+  open=None
+  high=None
+  low=None
+  close=None
   def __init__(self, date_):
     self.date = date_
 
@@ -74,26 +74,38 @@ class Algo:
 
     self.df_ = pd.read_sql(cmd_, db_connection)
 
+  def pre_process(self, time_end):
+    # update date content if necessary
+    time_end_p_ = time_end[:10]
+    if self.current_date_:
+      if time_end_p_ != self.current_date_.date:
+        self.log_day()
+        self.current_date_ = DayData(time_end_p_)
+    else:
+      self.current_date_ = DayData(time_end_p_)
+
+  def post_process(self, val):
+    # isolate supporting functionality to support function
+
+    # update current_date_.open if this is a new date
+    if self.current_date_.open == None:
+      self.current_date_.open = val
+
+    # update close; when date exists, last value will accurately rep last value
+    self.current_date_.close = val
+
   def process(self, time_end):
     # process data and generate a position at time_end (end of data by default)
+    self.pre_process(time_end)
+
     latest_ = self.df_[(self.df_['datetime'] == time_end)]
     val_ = latest_.iloc[0]["close"] # get 0 index, "close" attribute
     self.value_last_ = val_
 
-    # update date content if necessary
-    datetime_ = latest_.iloc[0]["datetime"].strftime('%Y-%m-%d')
-    precedent_date_ = ""
-    if self.current_date_:
-      precedent_date_ = self.current_date_.date
-    if datetime_ != precedent_date_:
-      self.log_day()
-      self.current_date_ = DayData(datetime_)
-      self.current_date_.open = val_
-
-    self.current_date_.close = val_
-
     if self.shares_== 0:
       self.buy(time_end,val_)
+
+    self.post_process(val_)
 
   def log_day(self):
     if self.current_date_ != None:
