3a3a4d993116681f9815f7820afcab19acb5d303
danny <dannytakushi@gmail.com>
2023-12-26 17:06:31 -0600

    transactionType enum

--- app_cpp/app_cpp/transaction.cpp
+++ app_cpp/app_cpp/transaction.cpp
@@ -2,18 +2,27 @@
 #include <iomanip>
 #include "transaction.h"
 transaction::transaction(std::time_t time, double value,
-                          std::string transaction){
+                          transactionType type){
   time_ = time;
   value_ = value;
-  transaction_ = transaction;
+  type_ = type;
 }
 
 void transaction::print(){
   std::cout << std::asctime(std::localtime(&time_)) << " : ";
-  std::cout << transaction_;
+  switch(type_){
+    case transactionType::kBuy:
+      std::cout << "buy";
+      break;
+    case transactionType::kSell:
+      std::cout << "sell";
+      break;
+    default:
+      std::cout << "unknown(?)";
+  }
   std::cout <<" at " << std::setprecision(3)<<value_<<std::endl;
 }
 
 std::time_t transaction::get_time(){return time_;}
 double transaction::get_value(){return value_;}
-std::string transaction::get_transaction(){return transaction_;}
+transactionType transaction::get_type(){return type_;}
--- app_cpp/app_cpp/transaction.h
+++ app_cpp/app_cpp/transaction.h
@@ -1,15 +1,18 @@
 #include <ctime>
 #include <string>
-
+enum class transactionType{
+  kBuy = 0,
+  kSell,
+};
 class transaction{
   std::time_t time_;
   double value_;
-  std::string transaction_ = "";
+  transactionType type_ = transactionType::kBuy;
 
 public:
-  transaction(std::time_t time, double value, std::string transaction);
+  transaction(std::time_t time, double value, transactionType type);
   void print();
   std::time_t get_time();
   double get_value();
-  std::string get_transaction();
+  transactionType get_type();
 };
