b3e88e5e824883c02fd0abbb49f5816487e951cd
danny <dannytakushi@gmail.com>
2023-12-26 22:33:11 -0600

    dayrecorder

---
+++ app_cpp/app_cpp/dayrecorder.cpp
@@ -0,0 +1,36 @@
+#include "dayrecorder.h"
+
+dayrecorder::dayrecorder(){
+  days_ = std::make_shared<std::vector<std::shared_ptr<daydata>>>();
+}
+
+ymd_date dayrecorder::get_current_date(){
+  /** returns 1900-01-01 if no date initialized**/
+  ymd_date out{std::chrono::January/1/1900};
+  if(days_->size()>0){
+    out = days_->back()->get_date();
+  }
+  return out;
+}
+
+void dayrecorder::start_new_day(ymd_date date){
+  days_->push_back(std::make_shared<daydata>(date));
+}
+
+void dayrecorder::set_current_open(double open){
+  if(days_->size()>0){
+    days_->back()->set_open(open);
+  }
+}
+
+void dayrecorder::set_current_close(double close){
+  if(days_->size()>0){
+    days_->back()->set_close(close);
+  }
+}
+
+void dayrecorder::set_current_portfolio_value(double portfolio_value){
+  if(days_->size()>0){
+    days_->back()->set_portfolio(portfolio_value);
+  }
+}
---
+++ app_cpp/app_cpp/dayrecorder.h
@@ -0,0 +1,15 @@
+#include "daydata.h"
+#include <chrono>
+#include <memory>
+#include <vector>
+class dayrecorder{
+protected:
+  std::shared_ptr<std::vector<std::shared_ptr<daydata>>> days_;
+public:
+  dayrecorder();
+  ymd_date get_current_date();
+  void start_new_day(ymd_date date);
+  void set_current_open(double open);
+  void set_current_close(double close);
+  void set_current_portfolio_value(double portfolio_value);
+};
