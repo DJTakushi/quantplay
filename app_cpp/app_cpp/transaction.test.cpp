5ef8e195563f4cd3411011a71a453450223904f0
danny <dannytakushi@gmail.com>
2023-12-26 19:51:28 -0600

    attempt to remove intellisense warnings

--- app_cpp/app_cpp/transaction.test.cpp
+++ app_cpp/app_cpp/transaction.test.cpp
@@ -1,7 +1,7 @@
 #include "transaction.h"
 #include <gtest/gtest.h>
 
-TEST(TransactionTest,constructor){
+TEST(TransactionTest,transactions_constructor) {
   std::tm tm{}; // Zero initialise
   tm.tm_year = 2020 - 1900; // 2020
   tm.tm_mon = 2 - 1; // February
@@ -10,9 +10,9 @@ TEST(TransactionTest,constructor){
   tm.tm_min = 15;
   tm.tm_isdst = 0; // Not daylight saving
   std::time_t time = std::mktime(&tm);
-  transaction* t = new transaction(time,12.34,"doot");
+  transaction* t = new transaction(time,12.34,transactionType::kBuy);
 
   EXPECT_EQ(time, t->get_time());
   EXPECT_DOUBLE_EQ(12.34,t->get_value());
-  EXPECT_EQ("doot",t->get_transaction());
+  EXPECT_EQ(transactionType::kBuy,t->get_type());
 }
