04b215a719d6b75c368007a051d6b2e968c78fae
DannyT470s <okinawanspud@gmail.com>
2024-02-11 13:21:51 -0600

    algo_data constructor; formatting++

--- app_cpp/app_cpp/algo.h
+++ app_cpp/app_cpp/algo.h
@@ -6,10 +6,13 @@ class algo{
     double open_= 0.0;
     double close_ = 0.0;
     int volume_ = 0;
+    algo_data(double open, double close, int volume) : open_(open),
+                                                      close_(close),
+                                                      volume_(volume){}
   };
   std::list<algo_data> data_;
 
+  public:
   void addData(double open, double close, int volume);
-
   order* process();
 };
