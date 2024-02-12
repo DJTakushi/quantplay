3a46ca5c153188ef7d9ad2c3698b9c08e2b8f0e9
DJTakushi <okinawanspud@gmail.com>
2024-02-11 20:39:47 -0600

    algo1_data_retriever

---
+++ app_cpp/app_cpp/algo/algo1/algo1_data_retriever.cpp
@@ -0,0 +1,7 @@
+#include "algo1_data_retriever.h"
+
+std::list<algo1_data> get_data(){
+  std::list<algo1_data> output;
+
+  return output;
+}
---
+++ app_cpp/app_cpp/algo/algo1/algo1_data_retriever.h
@@ -0,0 +1,5 @@
+#include <list>
+#include "algo1_data.h"
+class algo1_data_retriever{
+  std::list<algo1_data> get_data();
+};
