20d138f1ff171275b22f446c15f661fda2b6ce4e
DJTakushi <okinawanspud@gmail.com>
2024-02-12 21:33:37 -0600

    algo_manager

---
+++ app_cpp/app_cpp/algo_manager.cpp
@@ -0,0 +1,8 @@
+#include "algo1_controller.h"
+#include "portfolio.h"
+#include "dayrecorder.h"
+class algo_manager{
+  algo1_controller* algo1_controller_;
+  portfolio* portfolio_;
+  dayrecorder* recorder_;
+};
