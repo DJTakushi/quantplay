3053919752b9ab97ec70ed317622d670c4c3b23d
DJTakushi <okinawanspud@gmail.com>
2024-02-12 22:11:31 -0600

    abstract functions defined

--- app_cpp/app_cpp/algo/algo1/algo1_controller.h
+++ app_cpp/app_cpp/algo/algo1/algo1_controller.h
@@ -6,7 +6,7 @@ class algo1_controller : public algo_controller_i{
   algo1* algo;
 public:
   algo1_controller();
-  void update_data() = 0;
-  void update_data_single() = 0;
-  transaction* get_transaction() = 0;
+  void update_data();
+  void update_data_single();
+  transaction* get_transaction();
 };
