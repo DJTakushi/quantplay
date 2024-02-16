2bdeb2a8b6801c241b0b41f993365f5fff23b030
DJTakushi <okinawanspud@gmail.com>
2024-02-15 20:43:41 -0600

    create_table

--- app_cpp/app_cpp/algo/algo1/algo1_data_retriever.cpp
+++ app_cpp/app_cpp/algo/algo1/algo1_data_retriever.cpp
@@ -1,12 +1,22 @@
 #include "algo1_data_retriever.h"
 #include <iostream>
 algo1_data_retriever::algo1_data_retriever(sql::Connection* connection)
-    : connection_(connection){}
+    : connection_(connection){
+  create_database();
+}
 
 void algo1_data_retriever::create_database(){
   sql::Statement* stmnt =connection_->createStatement();
+  std::string cmd = "CREATE TABLE IF NOT EXISTS algo1 (";
+  cmd+="datetime DATETIME, ";
+  cmd+="open float(2), ";
+  cmd+="high float(2), ";
+  cmd+="low float(2), ";
+  cmd+="close float(2), ";
+  cmd+="volume int";
+  cmd+=");";
   try{
-    stmnt->executeUpdate("");
+    stmnt->executeUpdate(cmd);
   }
   catch (sql::SQLException& e) {
     std::cerr << "Error altering table: " << e.what() << std::endl;
