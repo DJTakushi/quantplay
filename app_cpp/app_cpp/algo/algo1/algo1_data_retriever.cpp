c87b4645c92d5726432ebaa63c69ea3cca946d14
DJTakushi <okinawanspud@gmail.com>
2024-02-15 19:45:29 -0600

    prototype create_database

--- app_cpp/app_cpp/algo/algo1/algo1_data_retriever.cpp
+++ app_cpp/app_cpp/algo/algo1/algo1_data_retriever.cpp
@@ -3,6 +3,9 @@
 algo1_data_retriever::algo1_data_retriever(sql::Connection* connection)
     : connection_(connection){}
 
+void algo1_data_retriever::create_database(){
+
+}
 
 std::list<algo1_data> algo1_data_retriever::get_data(){
   std::list<algo1_data> output;
@@ -10,4 +13,4 @@ std::list<algo1_data> algo1_data_retriever::get_data(){
   return output;
 }
 void algo1_data_retriever::update_database(){
-};
+}
--- app_cpp/app_cpp/algo/algo1/algo1_data_retriever.h
+++ app_cpp/app_cpp/algo/algo1/algo1_data_retriever.h
@@ -5,6 +5,7 @@ class algo1_data_retriever{
   sql::Connection* connection_;
   public:
   algo1_data_retriever(sql::Connection* connection);
+  void create_database();
   std::list<algo1_data> get_data();
   void update_database();
 };
