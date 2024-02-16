68798180d76e573d248e5c9531f418063ff8dc68
DJTakushi <okinawanspud@gmail.com>
2024-02-15 20:31:36 -0600

    create_database work

--- app_cpp/app_cpp/algo/algo1/algo1_data_retriever.cpp
+++ app_cpp/app_cpp/algo/algo1/algo1_data_retriever.cpp
@@ -1,10 +1,16 @@
 #include "algo1_data_retriever.h"
-
+#include <iostream>
 algo1_data_retriever::algo1_data_retriever(sql::Connection* connection)
     : connection_(connection){}
 
 void algo1_data_retriever::create_database(){
-
+  sql::Statement* stmnt =connection_->createStatement();
+  try{
+    stmnt->executeUpdate("");
+  }
+  catch (sql::SQLException& e) {
+    std::cerr << "Error altering table: " << e.what() << std::endl;
+  }
 }
 
 std::list<algo1_data> algo1_data_retriever::get_data(){
