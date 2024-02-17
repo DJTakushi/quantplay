f658e8c33b7fcc7c5a84b5061a1e1ae8ee16b4a6
DJTakushi <okinawanspud@gmail.com>
2024-02-16 23:29:23 -0600

    timestamp added to algo1 data

--- app_cpp/app_cpp/algo/algo1/algo1_data.h
+++ app_cpp/app_cpp/algo/algo1/algo1_data.h
@@ -1,9 +1,13 @@
 #pragma once
+#include <ctime>
 struct algo1_data{
+  std::time_t time_;
   double open_;
   double close_;
   int volume_;
-  algo1_data(double open, double close, int volume) : open_(open),
-                                                      close_(close),
-                                                      volume_(volume){};
+  algo1_data(std::time_t time, double open, double close, int volume)
+      : time_(time),
+      open_(open),
+      close_(close),
+      volume_(volume){};
 };
