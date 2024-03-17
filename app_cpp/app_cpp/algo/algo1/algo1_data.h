f26f678ba01c50cbb74320189d1b3e292992059f
DJTakushi <okinawanspud@gmail.com>
2024-03-17 01:02:32 -0500

    algo1_data default constructor; formatting++

--- app_cpp/app_cpp/algo/algo1/algo1_data.h
+++ app_cpp/app_cpp/algo/algo1/algo1_data.h
@@ -2,7 +2,13 @@
 #include "ohlcv.h"
 #include <ctime>
 struct algo1_data : public ohlcv{
-  algo1_data(std::time_t time, double open, double high, double low, double close, int volume)
-      : ohlcv(time,open,high,low,close,volume){};
+  algo1_data(std::time_t time,
+              double open,
+              double high,
+              double low,
+              double close,
+              int volume)
+              : ohlcv(time,open,high,low,close,volume){};
   algo1_data(algo1_data* d):ohlcv(d){}
+  algo1_data():ohlcv(){};
 };
