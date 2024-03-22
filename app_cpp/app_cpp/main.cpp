4d5d47764e9c0e7b06ecaa83f287ceb03b413bac
DJTakushi <okinawanspud@gmail.com>
2024-03-22 04:10:31 -0500

    derive sharpe ratio first

--- app_cpp/app_cpp/main.cpp
+++ app_cpp/app_cpp/main.cpp
@@ -29,7 +29,8 @@ int main(int argc, char** argv){
   // std::cout << "OHLCV Days :" << std::endl;
   // manager_->print_ohlcv_days();
 
-  std::cout << "Sharpe Ratio : " <<std::to_string(manager_->compute_sharpe_ratio())<<std::endl;
+  double sharpe_ratio = manager_->compute_sharpe_ratio();
+  std::cout << "Sharpe Ratio : " <<std::to_string(sharpe_ratio)<<std::endl;
 
   sql_connection_->close();
 }
