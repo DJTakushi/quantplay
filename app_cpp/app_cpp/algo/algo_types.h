5781a9c112e79632de3e3c6955d318fe0ea1959e
DJTakushi <okinawanspud@gmail.com>
2024-04-20 12:19:41 -0500

    default algo indices

--- app_cpp/app_cpp/algo/algo_types.h
+++ app_cpp/app_cpp/algo/algo_types.h
@@ -1,10 +1,13 @@
 #pragma once
+#include <cstdint>
 #include <map>
 #include <string>
 enum algo_type_k {
   algo1_k = 0,
+  algoMax_k=UINT16_MAX,
 };
 
 static std::map<int,std::string> algo_name{
-  {algo1_k,"algo1"}
+  {algo1_k,"algo1"},
+  {algoMax_k,"algo_default"}
 };
