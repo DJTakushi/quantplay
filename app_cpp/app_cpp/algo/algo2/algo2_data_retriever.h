a0cac63a561112639e3f6701ef48162abfbdcf7f
DJTakushi <okinawanspud@gmail.com>
2024-04-20 12:22:58 -0500

    algo2_data_retriever inherits algo2_data_retriever

--- app_cpp/app_cpp/algo/algo2/algo2_data_retriever.h
+++ app_cpp/app_cpp/algo/algo2/algo2_data_retriever.h
@@ -1,13 +1,8 @@
-#include <list>
-#include "helpers.h"
+#include "algo_data_retriever_i.h"
 #include "ohlcv.h"
-#include <mariadb/conncpp.hpp>
-#include <ctime>
-#include <filesystem>
 
 namespace fs = std::filesystem;
-class algo2_data_retriever{
-  sql::Connection* connection_;
+class algo2_data_retriever : public algo_data_retriever_i {
   std::time_t latest_datapoint_;
   void add_data_to_database(std::list<ohlcv> data_list,
       std::string timezone_s = "US/Eastern");
@@ -15,7 +10,7 @@ class algo2_data_retriever{
   algo2_data_retriever(sql::Connection* connection);
   void drop_datatable();
   void create_datatable();
-  std::list<ohlcv> get_next_data_from_database(int num = -1);
+  std::list<ohlcv*> get_next_data_from_database(int num = -1) override;
   // void update_database();//TODO
   void update_database_from_file(fs::path filepath);
   void update_database_from_json(std::string j);
