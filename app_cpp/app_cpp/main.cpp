0c3cf4a6e625349e90cfc4c57102bbe651fd5ea4
DJTakushi <okinawanspud@gmail.com>
2024-04-23 00:02:20 -0500

    remove commented-out content

--- app_cpp/app_cpp/main.cpp
+++ app_cpp/app_cpp/main.cpp
@@ -15,7 +15,6 @@ int main(int argc, char** argv){
   sql::Connection* sql_connection_ = driver->connect(url, properties);
 
   algo_manager* manager_ = new algo_manager(algo1_k,sql_connection_);
-  // manager_->update_database_from_file(fs::path("app_cpp/intraday_ibm.json"));
   manager_->update_database_from_file(fs::path("app_cpp/3_4_ige.csv"));
   manager_->process_all_by_update_data_step(1);
   manager_->all_snapshots_to_db();
