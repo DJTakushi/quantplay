1494e8e4b0c964d7c65fe1dcb4a18484e9f3f71b
DJTakushi <okinawanspud@gmail.com>
2024-04-25 01:12:48 -0500

    thread main

--- app_cpp/app_cpp/main.cpp
+++ app_cpp/app_cpp/main.cpp
@@ -1,5 +1,10 @@
-#include "algo_manager.h"
 #include <iostream>
+#include <signal.h>
+#include <thread>
+#include "algo_manager.h"
+static volatile bool active = false;
+void de_activate(int param){ active = false; }
+
 int main(int argc, char** argv){
   sql::Driver* driver = sql::mariadb::get_driver_instance();
 
@@ -15,10 +20,19 @@ int main(int argc, char** argv){
   sql::Connection* sql_connection_ = driver->connect(url, properties);
 
   algo_manager* manager_ = new algo_manager(algo1_k,sql_connection_);
-  manager_->update_database_from_file(fs::path("app_cpp/3_4_ige.csv"));
 
-  // TODO : create loop to get data and run this
-  manager_->process_all_by_update_data_step(1);
+  signal(SIGINT, de_activate);
+  active = true;
+  auto process_loop = std::thread([manager_]() {
+    while(active){
+      std::cout << "processing...  "<<std::endl;
+      manager_->update_database_from_file(fs::path("app_cpp/3_4_ige.csv"),1);
+      manager_->process_all_by_update_data_step();
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+  });
+  process_loop.join();
+
   manager_->all_snapshots_to_db();
   manager_->update_database_analysis();
 
