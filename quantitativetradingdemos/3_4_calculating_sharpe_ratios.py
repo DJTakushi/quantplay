b5fe6aa68dac095618f8b1c6a0f0d57be8965743
danny <dannytakushi@gmail.com>
2025-12-24 03:37:03 -0600

    finer debugging details for 3_4 example

--- quantitativetradingdemos/3_4_calculating_sharpe_ratios.py
+++ quantitativetradingdemos/3_4_calculating_sharpe_ratios.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import numpy as np
+from pathlib import Path
+
+pd.options.display.float_format = '{:.16f}'.format
+
 
 # read ige data into dataframe
-ige_ = pd.read_csv('data/3_4_ige.csv')
+script_dir = Path(__file__).parent.absolute()   
+ige_ = pd.read_csv(f'{script_dir}/data/3_4_ige.csv')
 
 # correct date to common datetime format
 ige_['Date']=pd.to_datetime(ige_['Date'])
@@ -18,13 +23,24 @@ ige_["Dailyret"]=(ige_["Adj Close"]-ige_["PrevClose"])/ige_["PrevClose"]
 ige_["ExcessDailyRet"]=ige_["Dailyret"]-(0.04/252)
 
 # calculate Sharpe Ratio
+total_ret = ige_["ExcessDailyRet"].sum()
+n = ige_["ExcessDailyRet"].count()
 avg_ = ige_["ExcessDailyRet"].mean()
+variance_= ige_["ExcessDailyRet"].var()
 std_ = ige_["ExcessDailyRet"].std()
 sharpe_ = np.sqrt(252) * avg_ / std_
 
 # print data
 print(ige_.to_string())
 print(ige_.info())
+
+print("Adj Close:"+ ige_["ExcessDailyRet"].to_string())
+print("total_ret_:"+str(total_ret))
+print("n_:"+str(n))
 print("avg_:"+str(avg_))
+print("variance_:"+str(variance_))
 print("std_:"+str(std_))
-print(sharpe_)
+print("sharpe_:"+str(sharpe_))
+
+print("min Adj Close:"+ str(ige_["Adj Close"].min()))
+print("max Adj Close:"+ str(ige_["Adj Close"].max()))
